f2b81d8034c70c3aaafbd4a0ae672e990d283f19
Carly Huang <c85615@gmail.com>
2021-01-10 14:58:13 -0800

    add day 1 part 2 ans

---
+++ 01.2.py
@@ -0,0 +1,16 @@
+import reader
+
+def get_fuel_req(mass):
+    current_fuel = mass // 3 - 2
+    if current_fuel <= 0:
+        return 0
+    return current_fuel + get_fuel_req(current_fuel)
+
+def get_fuel_req_sum(modules):
+    sum_ = 0
+    for module in modules:
+        sum_ += get_fuel_req(module)
+    return sum_
+
+modules = reader.get_int_input_line_by_line("./input/01.txt")
+print(get_fuel_req_sum(modules))
